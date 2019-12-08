d0b7664c9e415d08c8599585a18c62ebd19f1908
jzablin <jzablin@yahoo.com>
2019-12-07 23:00:05 -0500

    Add files via upload

--- plotter.py
+++ plotter.py
@@ -7,7 +7,7 @@ matplotlib.rcParams['font.family'] = 'serif'
 matplotlib.rcParams['font.sans-serif'] = 'stix'
 from mpl_toolkits.mplot3d import Axes3D
 
-def plotter(result, x,y,z, pos3,proj=3, tarr=[]):
+def plotter(result, x,y,z, pos3,proj=3, tarr=[], point='n'):
     x1=result[0:,0]
     y1=result[0:,1]
     z1=result[0:,2]
@@ -37,7 +37,7 @@ def plotter(result, x,y,z, pos3,proj=3, tarr=[]):
         ax = fig.add_subplot(111)
         ax.set_xlabel('x')
         ax.set_ylabel('y')
-        ax.set_title('Initial Positions, initial position of body 3=%.1f'%(pos3))
+        ax.set_title('Initial Positions, initial position of body 3=%.5f'%(pos3))
         if x==1:
             ax.plot(x1[0],y1[0], 'ro', label="Star 1", markersize=10)
         if y==1:
@@ -45,18 +45,31 @@ def plotter(result, x,y,z, pos3,proj=3, tarr=[]):
         if z==1:
             ax.plot(x3[0],y3[0], 'bo',label="Star 2", markersize=10)
     
-    if proj==2: #creates a 2D plot
+    if proj==2 and point=='n': #creates a 2D plot
         fig = plt.figure(figsize=(7,7))
         ax = fig.add_subplot(111)        
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_title('Motion of Bodies, initial position of body 3=%.5f'%(pos3))
         if x==1:
-            ax.plot(x1,y1, 'r', label="Star 1", markersize=10)
+            ax.plot(x1,y1, 'r', label="Star 1")
         if y==1:
             ax.plot(x2,y2, 'k', label="Planet")
         if z==1:
-            ax.plot(x3,y3, 'b',label="Star 2", markersize=10)
+            ax.plot(x3,y3, 'b',label="Star 2")
+            
+    if proj==2 and point=='y': #creates a 2D plot and makes points for stars
+        fig = plt.figure(figsize=(7,7))
+        ax = fig.add_subplot(111)        
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_title('Motion of Bodies, initial position of body 3=%.5f'%(pos3))
+        if x==1:
+            ax.plot(x1,y1, 'ro', label="Star 1", markersize=10)
+        if y==1:
+            ax.plot(x2,y2, 'k', label="Planet")
+        if z==1:
+            ax.plot(x3,y3, 'bo',label="Star 2", markersize=10)
             
     if proj==3: #creates a 3D plot
         fig = plt.figure(figsize=(7,7))
@@ -64,7 +77,7 @@ def plotter(result, x,y,z, pos3,proj=3, tarr=[]):
         ax.set_xlabel('x')
         ax.set_ylabel('y')
         ax.set_zlabel('z')
-        ax.set_title('Motion of Bodies, initial position of body 3=%.1f'%(pos3))
+        ax.set_title('Motion of Bodies, initial position of body 3=%.5f'%(pos3))
         if x==1:
             ax.plot(x1,y1,z1, 'b', label="Star 1")
         if y==1:
@@ -77,7 +90,7 @@ def plotter(result, x,y,z, pos3,proj=3, tarr=[]):
         ax = fig.add_subplot(111)
         ax.set_xlabel('time')
         ax.set_ylabel('velocity')
-        ax.set_title('Velocity of Planet, initial position of body 3=%.1f'%(pos3))
+        ax.set_title('Velocity of Planet, initial position of body 3=%.5f'%(pos3))
         if x==1:
             ax.plot(tarr, vx2, 'k', label="Planet")
     
